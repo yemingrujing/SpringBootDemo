b94be114f17ddd80c9a9f529eec246b423e5b5a1
yemingrujing <yemingrujing@users.noreply.github.com>
2018-09-06 15:53:19 +0800

    视频转码

---
+++ util/src/main/java/com/example/util/video/PrintStream.java
@@ -0,0 +1,31 @@
+package com.example.util.video;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @author Wei.Guang
+ * @create 2018-09-06 14:47
+ **/
+public class PrintStream extends Thread {
+    InputStream _is = null;
+    public PrintStream(InputStream is) {
+        _is = is;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (this != null) {
+                int _ch = _is.read();
+                if (_ch != -1) {
+                    System.out.println((char) _ch);
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
