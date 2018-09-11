170477b1408fcb072d41b74eec2165b82944dc78
yemingrujing <yemingrujing@users.noreply.github.com>
2018-09-11 10:44:08 +0800

    maven编译失败

--- controller/src/main/java/com/example/ControllerApplication.java
+++ controller/src/main/java/com/example/ControllerApplication.java
@@ -2,14 +2,15 @@ package com.example;
 
 import com.example.util.common.SpringUtil;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 @EnableAsync
 @EnableAspectJAutoProxy
 @Import(SpringUtil.class)
