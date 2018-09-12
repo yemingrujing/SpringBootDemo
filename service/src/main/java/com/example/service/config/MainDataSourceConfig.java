61459e8514453adac8fccfb6dc2c431bba99271a
Wei.Guang <yemingrujing@163.com>
2018-09-12 22:42:57 +0800

    mybatis报找不到 type alias 对应的实体类的问题

--- service/src/main/java/com/example/service/config/MainDataSourceConfig.java
+++ service/src/main/java/com/example/service/config/MainDataSourceConfig.java
@@ -1,9 +1,11 @@
 package com.example.service.config;
 
+import org.apache.ibatis.io.VFS;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
@@ -35,11 +37,13 @@ public class MainDataSourceConfig {
     @Bean(name = "mainSqlSessionFactory")
     @Primary
     public SqlSessionFactory mainSqlSessionFactory(@Qualifier("mainDataSource") DataSource dataSource) throws Exception {
+        //DefaultVFS在获取jar上存在问题，使用springboot只能修改
+        VFS.addImplClass(SpringBootVFS.class);
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
         bean.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath*:mapper/**/*.xml"));
         //实体类可以在mapper.xml中使用别名
-        bean.setTypeAliasesPackage("com.example.base");
+        bean.setTypeAliasesPackage("com.example.base.login,com.example.base.order");
         return bean.getObject();
     }
 
