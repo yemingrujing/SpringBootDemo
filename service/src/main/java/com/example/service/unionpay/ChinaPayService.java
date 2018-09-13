199146db458b97b8606dc41f27c450b11d9905c2
Wei.Guang <yemingrujing@163.com>
2018-09-13 23:43:02 +0800

    chinapay支付

---
+++ service/src/main/java/com/example/service/unionpay/ChinaPayService.java
@@ -0,0 +1,14 @@
+package com.example.service.unionpay;
+
+/**
+ * chinapay支付
+ */
+public interface ChinaPayService {
+
+    /**
+     * 查询签约接口
+     * @param cardNo
+     * @return
+     */
+    String querySign(String cardNo);
+}
---
+++ service/src/main/java/com/example/service/unionpay/impl/ChinaPayServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.service.unionpay.impl;
+
+import com.example.service.unionpay.ChinaPayService;
+
+public class ChinaPayServiceImpl implements ChinaPayService {
+
+    @Override
+    public String querySign(String cardNo) {
+        
+        return null;
+    }
+}
