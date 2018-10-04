1937b6ad253af2caa07a6341a3cd41efe9243756
Wei.Guang <yemingrujing@163.com>
2018-10-04 22:25:30 +0800

    增加https访问

--- controller/src/main/java/com/example/ControllerApplication.java
+++ controller/src/main/java/com/example/ControllerApplication.java
@@ -1,16 +1,23 @@
 package com.example;
 
+import cn.hutool.core.io.resource.ClassPathResource;
 import com.example.util.common.SpringUtil;
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
-@SpringBootApplication
+import java.io.File;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableAsync
 @EnableAspectJAutoProxy
 @Import(SpringUtil.class)
@@ -18,5 +25,40 @@ public class ControllerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ControllerApplication.class, args);
+    }
+
+    @Value("${https.port}")
+    private Integer port;
+
+    @Value("${https.ssl.key-store-password}")
+    private String key_store_password;
+
+    @Value("${https.ssl.key-password}")
+    private String key_password;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+        try {
+            File keystore = new ClassPathResource("properties/keystore.p12").getFile();
+            connector.setScheme("https");
+            connector.setSecure(true);
+            connector.setPort(port);
+            protocol.setSSLEnabled(true);
+            protocol.setKeystoreFile(keystore.getAbsolutePath());
+            protocol.setKeystorePass(key_store_password);
+            protocol.setKeyPass(key_password);
+            return connector;
+        } catch (Exception e) {
+            throw new IllegalStateException("can't access keystore: [" + "keystore"
+                    + "] or truststore: [" + "keystore" + "]", e);
+        }
     }
 }
