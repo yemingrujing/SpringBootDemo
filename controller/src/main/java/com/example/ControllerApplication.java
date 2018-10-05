9e10651db52a4a9bd0bb3ae94ad4fc7639378c58
Wei.Guang <yemingrujing@163.com>
2018-10-05 14:34:02 +0800

    增加https访问

--- controller/src/main/java/com/example/ControllerApplication.java
+++ controller/src/main/java/com/example/ControllerApplication.java
@@ -1,10 +1,8 @@
 package com.example;
 
-import cn.hutool.core.io.resource.ClassPathResource;
 import com.example.util.common.SpringUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.connector.Connector;
-import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,8 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
-
-import java.io.File;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableAsync
@@ -29,14 +25,8 @@ public class ControllerApplication {
         SpringApplication.run(ControllerApplication.class, args);
     }
 
-    @Value("${https.port}")
+    @Value("${http.port}")
     private Integer port;
-
-    @Value("${https.ssl.key-store-password}")
-    private String key_store_password;
-
-    @Value("${https.ssl.key-password}")
-    private String key_password;
 
     @Bean
     public ServletWebServerFactory servletContainer() {
@@ -47,22 +37,7 @@ public class ControllerApplication {
 
     private Connector createSslConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
-        try {
-            log.info("================================================================================");
-            String keystore = Thread.currentThread().getContextClassLoader().getResource("properties/keystore.p12").getPath();
-            log.info("================================================================================");
-            connector.setScheme("https");
-            connector.setSecure(true);
-            connector.setPort(port);
-            protocol.setSSLEnabled(true);
-            protocol.setKeystoreFile(keystore);
-            protocol.setKeystorePass(key_store_password);
-            protocol.setKeyPass(key_password);
-            return connector;
-        } catch (Exception e) {
-            throw new IllegalStateException("can't access keystore: [" + "keystore"
-                    + "] or truststore: [" + "keystore" + "]", e);
-        }
+        connector.setPort(port);
+        return connector;
     }
 }
